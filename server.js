0b0353f4d89d966567df32b819aa71bc3df3f3f6
sudan bhusal <sudanbhusal20215@gmail.com>
2021-02-16 21:51:52 -0800

    completed

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`Listening at http://localhost:${PORT}!`);
 });
